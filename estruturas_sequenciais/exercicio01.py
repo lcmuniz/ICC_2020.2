764bd60c88a7ddab9b71a13c91295ff6b9e42101
= <=>
2020-10-01 10:35:01 -0300

    lcmuniz

--- estruturas_sequenciais/exercicio01.py
+++
@@ -1,4 +0,0 @@
-# Faça um programa que mostre a mensagem "Alô Mundo" na tela.
-
-print("Alô Mundo")
-
--- estruturas_sequenciais/exercicio02.py
+++
@@ -1,5 +0,0 @@
-# Faça um programa que peça um número e então mostre a mensagem O número informado foi [número].
-
-numero = input("Digite um número: ")
-
-print("O número informado foi", numero)
